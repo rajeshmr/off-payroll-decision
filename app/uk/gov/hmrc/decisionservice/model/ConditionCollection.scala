e54619292a300e0a83cfc89d399cb41bb54fc2f3
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2016-11-03 18:23:33 +0000

    2 sections example working

--- app/uk/gov/hmrc/decisionservice/model/ConditionCollection.scala
+++ app/uk/gov/hmrc/decisionservice/model/ConditionCollection.scala
@@ -2,20 +2,27 @@ package uk.gov.hmrc.decisionservice.model
 
 abstract class ConditionCollection(conditions: Map[String,String]) {
   def is(condition:String):String = conditions.getOrElse(condition, "undefined")
-  var carryOver:String = "" // TODO remove
 }
 
+trait CarryOver
+
 case class Substitution(conditions: Map[String,String]) extends ConditionCollection(conditions)
+case class SubstitutionCO(carryOver:String) extends CarryOver
 
 case class Helper(conditions: Map[String,String]) extends ConditionCollection(conditions)
+case class HelperCO(carryOver:String) extends CarryOver
 
 case class Control(conditions: Map[String,String]) extends ConditionCollection(conditions)
+case class ControlCO(carryOver:String) extends CarryOver
 
 case class FinancialRisk(conditions: Map[String,String]) extends ConditionCollection(conditions)
+case class FinancialRiskCO(carryOver:String) extends CarryOver
 
 case class BusinessStructure(conditions: Map[String,String]) extends ConditionCollection(conditions)
+case class BusinessStructureCO(carryOver:String) extends CarryOver
 
 case class PartOfOrganization(conditions: Map[String,String]) extends ConditionCollection(conditions)
+case class PartOfOrganizationCO(carryOver:String) extends CarryOver
 
 case class Decision(text:String)
 
--- test/uk/gov/hmrc/decisionservice/RulesExecutorSpec.scala
+++ test/uk/gov/hmrc/decisionservice/RulesExecutorSpec.scala
@@ -3,7 +3,7 @@ package uk.gov.hmrc.decisionservice
 import org.junit.runner.RunWith
 import org.scalatest.{FunSuite, ShouldMatchers}
 import org.scalatest.junit.JUnitRunner
-import uk.gov.hmrc.decisionservice.model.{BusinessStructure, Decision, Substitution}
+import uk.gov.hmrc.decisionservice.model.{BusinessStructure, CarryOver, Decision, Substitution}
 import uk.gov.hmrc.decisionservice.service.RulesExecutor
 
 import collection.JavaConversions._
@@ -23,9 +23,12 @@ class RulesExecutorSpec extends FunSuite with ShouldMatchers {
 
   test("basic testcase") {
     val found = RulesExecutor.analyze(model1, "sheets/kb-rules-01.xls")
-    val decisions = found collect { case x:Decision => x}
-    found should have size(3)
+    val carryOvers = found collect { case x:CarryOver => x}
+    val decisions = found collect { case x:Decision => x }
+    found should have size(5)
+    carryOvers should have size(2)
     decisions should have size(1)
+    carryOvers.foreach{println(_)}
     decisions.foreach{println(_)}
   }
 
