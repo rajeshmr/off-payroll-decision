51cf98c9b20a716b3cf22b3473d8877b423225c7
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2016-11-03 18:28:06 +0000

    improved sheet readability

--- app/uk/gov/hmrc/decisionservice/model/ConditionCollection.scala
+++ app/uk/gov/hmrc/decisionservice/model/ConditionCollection.scala
@@ -7,22 +7,22 @@ abstract class ConditionCollection(conditions: Map[String,String]) {
 trait CarryOver
 
 case class Substitution(conditions: Map[String,String]) extends ConditionCollection(conditions)
-case class SubstitutionCO(carryOver:String) extends CarryOver
+case class SubstitutionCarryOver(value:String) extends CarryOver
 
 case class Helper(conditions: Map[String,String]) extends ConditionCollection(conditions)
-case class HelperCO(carryOver:String) extends CarryOver
+case class HelperCarryOver(value:String) extends CarryOver
 
 case class Control(conditions: Map[String,String]) extends ConditionCollection(conditions)
-case class ControlCO(carryOver:String) extends CarryOver
+case class ControlCarryOver(value:String) extends CarryOver
 
 case class FinancialRisk(conditions: Map[String,String]) extends ConditionCollection(conditions)
-case class FinancialRiskCO(carryOver:String) extends CarryOver
+case class FinancialRiskCarryOver(value:String) extends CarryOver
 
 case class BusinessStructure(conditions: Map[String,String]) extends ConditionCollection(conditions)
-case class BusinessStructureCO(carryOver:String) extends CarryOver
+case class BusinessStructureCarryOver(value:String) extends CarryOver
 
 case class PartOfOrganization(conditions: Map[String,String]) extends ConditionCollection(conditions)
-case class PartOfOrganizationCO(carryOver:String) extends CarryOver
+case class PartOfOrganizationCarryOver(value:String) extends CarryOver
 
 case class Decision(text:String)
 
