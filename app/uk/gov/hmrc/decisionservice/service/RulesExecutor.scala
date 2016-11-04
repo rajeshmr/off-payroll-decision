5d96fd35cfaf579c3e2714a2603448e0894ee41a
REDACTED <REDACTED>
2016-11-04 15:47:00 +0000

    converted exceptions to Xor, unit tests

--- app/uk/gov/hmrc/decisionservice/service/RulesExecutor.scala
+++ app/uk/gov/hmrc/decisionservice/service/RulesExecutor.scala
@@ -1,12 +1,13 @@
 package uk.gov.hmrc.decisionservice.service
 
-import java.util
-
+import cats.data.Xor
 import org.drools.builder.{KnowledgeBuilderFactory, ResourceType}
 import org.drools.io.ResourceFactory
 import org.slf4j.LoggerFactory
+import uk.gov.hmrc.decisionservice.model.{DecisionServiceError, KnowledgeBaseError, RulesFileError}
 
 import scala.collection.JavaConversions._
+import scala.util.{Failure, Success, Try}
 
 object RulesExecutor {
   val logger = LoggerFactory.getLogger(RulesExecutor.getClass())
@@ -16,31 +17,37 @@ object RulesExecutor {
     try doit(res) finally res.dispose
   }
 
-  def analyze(model: List[Any], kb: String):util.Collection[AnyRef] = {
-    System.setProperty("drools.dialect.java.compiler", "JANINO")
+  def analyze(model: List[Any], kb: String):Xor[DecisionServiceError,List[AnyRef]] = {
 
-    val config = KnowledgeBuilderFactory.newKnowledgeBuilderConfiguration()
-    config.setProperty("drools.dialect.mvel.strict", "false")
-    val kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder(config)
-
-    val res = ResourceFactory.newClassPathResource(kb)
-    kbuilder.add(res, ResourceType.DTABLE)
-
-    val errors = kbuilder.getErrors();
-    if (errors.size() > 0) {
-      for (error <- errors) logger.error(error.getMessage())
-      throw new IllegalArgumentException("Problem with the Knowledge base");
+    val tryRes = Try {
+      System.setProperty("drools.dialect.java.compiler", "JANINO")
+      val config = KnowledgeBuilderFactory.newKnowledgeBuilderConfiguration()
+      config.setProperty("drools.dialect.mvel.strict", "false")
+      val res = ResourceFactory.newClassPathResource(kb)
+      val knowledgeBuilder = KnowledgeBuilderFactory.newKnowledgeBuilder(config)
+      knowledgeBuilder.add(res, ResourceType.DTABLE)
+      knowledgeBuilder
     }
-
-    val kbase = kbuilder.newKnowledgeBase()
-
-    val results = using(kbase.newStatefulKnowledgeSession()) { session =>
-      session.setGlobal("logger", LoggerFactory.getLogger(kb))
-      model.foreach(session.insert(_))
-      session.fireAllRules()
-      session.getObjects()
+    tryRes match {
+      case Success(kbuilder) =>
+        val errors = kbuilder.getErrors()
+        errors.size() match {
+          case n if n > 0 =>
+            for (error <- errors) logger.error(error.getMessage())
+            Xor.left(KnowledgeBaseError(s"Problem(s) with knowledge base $errors"))
+          case _ =>
+            val kbase = kbuilder.newKnowledgeBase()
+            val results = using(kbase.newStatefulKnowledgeSession()) { session =>
+              session.setGlobal("logger", LoggerFactory.getLogger(kb))
+              model.foreach(session.insert(_))
+              session.fireAllRules()
+              session.getObjects()
+            }
+            Xor.right(results.toList)
+        }
+      case Failure(e) =>
+        Xor.left(RulesFileError(e.getMessage))
     }
-
-    results
   }
+
 }
--- test/uk/gov/hmrc/decisionservice/RulesExecutorSpec.scala
+++ test/uk/gov/hmrc/decisionservice/RulesExecutorSpec.scala
@@ -2,11 +2,9 @@ package uk.gov.hmrc.decisionservice
 
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.{BeforeAndAfterEach, Inspectors, LoneElement}
-import uk.gov.hmrc.decisionservice.model.{BusinessStructure, CarryOver, Decision, Substitution}
+import uk.gov.hmrc.decisionservice.model._
 import uk.gov.hmrc.decisionservice.service.RulesExecutor
 import uk.gov.hmrc.play.test.UnitSpec
-
-import scala.collection.JavaConversions._
 
 class RulesExecutorSpec extends UnitSpec with BeforeAndAfterEach with ScalaFutures with LoneElement with Inspectors with IntegrationPatience {
 
@@ -16,37 +14,65 @@ class RulesExecutorSpec extends UnitSpec with BeforeAndAfterEach with ScalaFutur
         Substitution(Map("rightToSendSubstituteInContract" -> "yes", "obligationToSendSubstituteInContract" -> "yes")),
         BusinessStructure(Map("advertiseForWork" -> "yes", "expenseRunningBusinessPremises" -> "yes"))
       )
-      val found = RulesExecutor.analyze(model, "rules-test-basic.xls")
-      val carryOvers = found collect { case x:CarryOver => x}
-      val decisions = found collect { case x:Decision => x }
-      found should have size(5)
-      carryOvers should have size(2)
-      decisions should have size(1)
-      (carryOvers map { _.value }).toList should contain theSameElementsAs List("high", "out")
-      (decisions map {_.text}).head should equal ("out of IR35")
+      val result = RulesExecutor.analyze(model, "rules-test-basic.xls")
+      result.isRight shouldBe true
+      result map
+      { found =>
+        val carryOvers = found collect { case x: CarryOver => x }
+        val decisions = found collect { case x: Decision => x }
+        found should have size (5)
+        carryOvers should have size (2)
+        decisions should have size (1)
+        (carryOvers map {
+          _.value
+        }) should contain theSameElementsAs List("high", "out")
+        (decisions map {
+          _.text
+        }).head should equal("out of IR35")
+      }
     }
     "treat section empty inputs as always matching" in {
       val model = List(
         BusinessStructure(Map("advertiseForWork" -> "no"))
       )
-      val found = RulesExecutor.analyze(model, "rules-test-emptyvalues-section.xls")
-      val carryOvers = found collect { case x:CarryOver => x}
-      found should have size(2)
-      carryOvers should have size(1)
-      (carryOvers map { _.value }).head should equal ("low")
+      val result = RulesExecutor.analyze(model, "rules-test-emptyvalues-section.xls")
+      result.isRight shouldBe true
+      result map { found =>
+        val carryOvers = found collect { case x: CarryOver => x }
+        found should have size (2)
+        carryOvers should have size (1)
+        (carryOvers map {
+          _.value
+        }).head should equal("low")
+      }
     }
     "treat decision empty inputs as always matching" in {
       val model = List(
         Substitution(Map("rightToSendSubstituteInContract" -> "yes", "obligationToSendSubstituteInContract" -> "yes")),
         BusinessStructure(Map("advertiseForWork" -> "yes", "expenseRunningBusinessPremises" -> "yes"))
       )
-      val found = RulesExecutor.analyze(model, "rules-test-emptyvalues-decision.xls")
-      val carryOvers = found collect { case x:CarryOver => x}
-      val decisions = found collect { case x:Decision => x }
-      found should have size(5)
-      carryOvers should have size(2)
-      decisions should have size(1)
-      (decisions map {_.text}).head should equal ("out")
+      val result = RulesExecutor.analyze(model, "rules-test-emptyvalues-decision.xls")
+      result.isRight shouldBe true
+      result map { found =>
+        val carryOvers = found collect { case x: CarryOver => x }
+        val decisions = found collect { case x: Decision => x }
+        found should have size (5)
+        carryOvers should have size (2)
+        decisions should have size (1)
+        (decisions map {
+          _.text
+        }).head should equal("out")
+      }
+    }
+  }
+
+  "processing not existing spreadsheet" should {
+    "produce error" in {
+      val result = RulesExecutor.analyze(List(), "not-existing.xls")
+      result.isLeft shouldBe true
+      result.leftMap(e =>
+        e shouldBe a [RulesFileError]
+      )
     }
   }
 
