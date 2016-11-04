448a4233a13b181df15d5463a974a801903a96f4
REDACTED <REDACTED>
2016-11-04 14:06:00 +0000

    empty values tests

--- test/uk/gov/hmrc/decisionservice/RulesExecutorSpec.scala
+++ test/uk/gov/hmrc/decisionservice/RulesExecutorSpec.scala
@@ -1,36 +1,53 @@
 package uk.gov.hmrc.decisionservice
 
-import org.junit.runner.RunWith
-import org.scalatest.{FunSuite, ShouldMatchers}
-import org.scalatest.junit.JUnitRunner
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.{BeforeAndAfterEach, Inspectors, LoneElement}
 import uk.gov.hmrc.decisionservice.model.{BusinessStructure, CarryOver, Decision, Substitution}
 import uk.gov.hmrc.decisionservice.service.RulesExecutor
+import uk.gov.hmrc.play.test.UnitSpec
 
-import collection.JavaConversions._
+import scala.collection.JavaConversions._
 
-@RunWith(classOf[JUnitRunner])
-class RulesExecutorSpec extends FunSuite with ShouldMatchers {
+class RulesExecutorSpec extends UnitSpec with BeforeAndAfterEach with ScalaFutures with LoneElement with Inspectors with IntegrationPatience {
 
-  def model1 = {
-    val substitution = Substitution(Map("rightToSendSubstituteInContract" -> "yes", "obligationToSendSubstituteInContract" -> "yes"))
-    val businessStructure = BusinessStructure(Map("advertiseForWork" -> "yes", "expenseRunningBusinessPremises" -> "yes"))
-
-    List(
-      substitution,
-      businessStructure
-    )
+  "processing rules" should {
+    "return expected facts" in {
+      val model = List(
+        Substitution(Map("rightToSendSubstituteInContract" -> "yes", "obligationToSendSubstituteInContract" -> "yes")),
+        BusinessStructure(Map("advertiseForWork" -> "yes", "expenseRunningBusinessPremises" -> "yes"))
+      )
+      val found = RulesExecutor.analyze(model, "rules-test-basic.xls")
+      val carryOvers = found collect { case x:CarryOver => x}
+      val decisions = found collect { case x:Decision => x }
+      found should have size(5)
+      carryOvers should have size(2)
+      decisions should have size(1)
+      (carryOvers map { _.value }).toList should contain theSameElementsAs List("high", "out")
+      (decisions map {_.text}).head should equal ("out of IR35")
+    }
+    "treat section empty inputs as always matching" in {
+      val model = List(
+        BusinessStructure(Map("advertiseForWork" -> "no"))
+      )
+      val found = RulesExecutor.analyze(model, "rules-test-emptyvalues-section.xls")
+      val carryOvers = found collect { case x:CarryOver => x}
+      found should have size(2)
+      carryOvers should have size(1)
+      (carryOvers map { _.value }).head should equal ("low")
+    }
+    "treat decision empty inputs as always matching" in {
+      val model = List(
+        Substitution(Map("rightToSendSubstituteInContract" -> "yes", "obligationToSendSubstituteInContract" -> "yes")),
+        BusinessStructure(Map("advertiseForWork" -> "yes", "expenseRunningBusinessPremises" -> "yes"))
+      )
+      val found = RulesExecutor.analyze(model, "rules-test-emptyvalues-decision.xls")
+      val carryOvers = found collect { case x:CarryOver => x}
+      val decisions = found collect { case x:Decision => x }
+      found should have size(5)
+      carryOvers should have size(2)
+      decisions should have size(1)
+      (decisions map {_.text}).head should equal ("out")
+    }
   }
-
-  test("basic testcase") {
-    val found = RulesExecutor.analyze(model1, "sheets/kb-rules-01.xls")
-    val carryOvers = found collect { case x:CarryOver => x}
-    val decisions = found collect { case x:Decision => x }
-    found should have size(5)
-    carryOvers should have size(2)
-    decisions should have size(1)
-    carryOvers.foreach{println(_)}
-    decisions.foreach{println(_)}
-  }
-
 
 }
