0ca52cf6b1d939d4a204bf7c6186296ec21df15f
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2016-11-08 10:23:27 +0000

    Drools sequential and parallel tests

--- test/uk/gov/hmrc/decisionservice/RulesExecutorPerformanceSpec.scala
+++ test/uk/gov/hmrc/decisionservice/RulesExecutorPerformanceSpec.scala
@@ -8,22 +8,41 @@ import org.scalatest.{BeforeAndAfterEach, Inspectors, LoneElement}
 import uk.gov.hmrc.decisionservice.model._
 import uk.gov.hmrc.decisionservice.service.RulesExecutor
 import uk.gov.hmrc.play.test.UnitSpec
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+
+import scala.concurrent.Future
 
 class RulesExecutorPerformanceSpec extends UnitSpec with BeforeAndAfterEach with ScalaFutures with LoneElement with Inspectors with IntegrationPatience {
 
-  "processing rules" should {
-    "return expected facts" in {
-      val model = List(
-        Substitution(Map("rightToSendSubstituteInContract" -> "yes", "obligationToSendSubstituteInContract" -> "yes")),
-        BusinessStructure(Map("advertiseForWork" -> "yes", "expenseRunningBusinessPremises" -> "yes"))
-      )
+  val model = List(
+    Substitution(Map("rightToSendSubstituteInContract" -> "yes", "obligationToSendSubstituteInContract" -> "yes")),
+    BusinessStructure(Map("advertiseForWork" -> "yes", "expenseRunningBusinessPremises" -> "yes"))
+  )
+
+  implicit val timeout = 20 seconds
+
+  "processing rules in parallel" should {
+    "return within a specified time limit" in {
       val start = nanoTime
       val futures = (1 to 200) map { a =>
         RulesExecutor.analyze(model, "rules-test-basic.xls")
       }
-      futures.foreach(await(_))
+      await(Future.sequence(futures))
       val duration = nanoTime - start
-      println("completed in %,d ms.".format(NANOSECONDS.toMillis(duration)))
+      println("parallel execution completed in %,d ms.".format(NANOSECONDS.toMillis(duration)))
+    }
+  }
+
+  "processing rules sequentially" should {
+    "return within a specified time limit" in {
+      val start = nanoTime
+      val futures = (1 to 200) map { a =>
+        await(RulesExecutor.analyze(model, "rules-test-basic.xls"))
+      }
+      val duration = nanoTime - start
+      println("sequential execution completed in %,d ms.".format(NANOSECONDS.toMillis(duration)))
     }
   }
 
