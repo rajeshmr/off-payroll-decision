9ea19fd96de9dd3dd14c987b7b8d15c8fb9a1a6f
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2016-11-09 18:31:28 +0000

    scala spike rulesfileerror test

--- test/uk/gov/hmrc/decisionservice/FactMatcherSpec.scala
+++ test/uk/gov/hmrc/decisionservice/FactMatcherSpec.scala
@@ -50,6 +50,25 @@ class FactMatcherSpec extends UnitSpec with BeforeAndAfterEach with ScalaFutures
         error shouldBe a [FactError]
       }
     }
+    "produce error when match not found" in {
+      val fact = Fact(List(
+        FactRow("question1", "yes"),
+        FactRow("question2", "no"),
+        FactRow("question3", "yes")), "BusinessStructure")
+      val rule = Rule(List(
+        RuleRow(List("yes","yes","yes"), SectionResult("high"  , true)),
+        RuleRow(List("yes","no" ,"no" ), SectionResult("medium", true)),
+        RuleRow(List("no" ,""   ,"yes"), SectionResult("low"   , false))
+      ))
+
+      val response = FactMatcher.matchSectionFact(fact:Fact, rule:Rule)
+
+      response shouldBe a [Xor[DecisionServiceError,SectionResult]]
+      response.isLeft shouldBe true
+      response.leftMap { error =>
+        error shouldBe a [RulesFileError]
+      }
+    }
   }
 
 }
