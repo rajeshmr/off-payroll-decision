64ab2f3fc30c06cf2eeded503ae823fa7b21a6e4
REDACTED <REDACTED>
2016-11-09 12:16:07 +0000

    switched to Drools 6.5.0.Final

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -41,7 +41,7 @@ trait MicroService {
       addTestReportOption(IntegrationTest, "int-test-reports"),
       testGrouping in IntegrationTest := oneForkedJvmPerTest((definedTests in IntegrationTest).value),
       parallelExecution in IntegrationTest := false)
-    .settings(resolvers += Resolver.bintrayRepo("hmrc", "releases"))
+    .settings(resolvers ++= Seq(Resolver.bintrayRepo("hmrc", "releases"), Resolver.jcenterRepo))
 }
 
 private object TestPhases {
