5e3b02fa20ac26ba98216d849e74e591e63261f1
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2016-11-10 11:38:43 +0000

    test for partial matrix fact

--- test/uk/gov/hmrc/decisionservice/MatrixFactMatcherSpec.scala
+++ test/uk/gov/hmrc/decisionservice/MatrixFactMatcherSpec.scala
@@ -2,8 +2,8 @@ package uk.gov.hmrc.decisionservice
 
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.{BeforeAndAfterEach, Inspectors, LoneElement}
-import uk.gov.hmrc.decisionservice.model._
-import uk.gov.hmrc.decisionservice.ruleengine.{MatrixFactMatcher, SectionFactMatcher}
+import uk.gov.hmrc.decisionservice.model.{SectionCarryOver, _}
+import uk.gov.hmrc.decisionservice.ruleengine.MatrixFactMatcher
 import uk.gov.hmrc.play.test.UnitSpec
 
 class MatrixFactMatcherSpec extends UnitSpec with BeforeAndAfterEach with ScalaFutures with LoneElement with Inspectors with IntegrationPatience {
@@ -24,6 +24,25 @@ class MatrixFactMatcherSpec extends UnitSpec with BeforeAndAfterEach with ScalaF
       response.isRight shouldBe true
       response.map { decision =>
         decision.value should equal("out of IR35")
+      }
+    }
+    "produce correct result for a partial fact" in {
+      val matrixFacts = MatrixFacts(List(
+        MatrixFact("BusinessStructure", SectionCarryOver("high", true)),
+        MatrixFact("Substitute", SectionCarryOver("low" , false)),
+        MatrixFact("FinancialRisk", SectionCarryOver("" , false))
+      ))
+      val matrixRules = List(
+        MatrixRule(List(SectionCarryOver("high"  , true ),SectionCarryOver("low" , true ),SectionCarryOver("low" , true )), MatrixDecision("self employed")),
+        MatrixRule(List(SectionCarryOver("high"  , true ),SectionCarryOver("low" , false),SectionCarryOver("" , true )), MatrixDecision("in IR35")),
+        MatrixRule(List(SectionCarryOver("medium", true ),SectionCarryOver("high", true ),SectionCarryOver("low" , true )), MatrixDecision("out of IR35"))
+      )
+
+      val response = MatrixFactMatcher.matchMatrixFacts(matrixFacts, matrixRules)
+
+      response.isRight shouldBe true
+      response.map { decision =>
+        decision.value should equal("in IR35")
       }
     }
   }
