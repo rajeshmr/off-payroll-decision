e45f9d2299c6ad94c6146d7d9afc2f354153eb03
REDACTED <REDACTED>
2016-11-10 12:37:45 +0000

    streamlined model

--- app/uk/gov/hmrc/decisionservice/model/SectionFacts.scala
+++ app/uk/gov/hmrc/decisionservice/model/SectionFacts.scala
@@ -6,9 +6,7 @@ case class SectionFacts(interview:List[SectionFact], sectionName:String){
   def values = interview.map(_.answer)
 }
 
-case class SectionRule(answers:List[String], result:SectionCarryOver){
-  def values = answers
-}
+case class SectionRule(values:List[String], result:SectionCarryOver)
 
 case class SectionRules(rows:List[SectionRule])
 
@@ -23,8 +21,5 @@ case class MatrixFacts(facts:List[MatrixFact]){
   def values = facts.map(_.carryOver)
 }
 
-case class MatrixRule(carryOvers:List[SectionCarryOver], decision:MatrixDecision){
-  def values = carryOvers
-  def result = decision
-}
+case class MatrixRule(values:List[SectionCarryOver], result:MatrixDecision)
 
