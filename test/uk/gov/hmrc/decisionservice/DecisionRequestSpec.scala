5ab5182eda0c77dffc659ebd7817af0b94a82161
REDACTED <REDACTED>
2016-11-15 11:31:09 +0000

    api model change

--- test/uk/gov/hmrc/decisionservice/DecisionRequestSpec.scala
+++ test/uk/gov/hmrc/decisionservice/DecisionRequestSpec.scala
@@ -3,7 +3,7 @@ package uk.gov.hmrc.decisionservice
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.{BeforeAndAfterEach, Inspectors, LoneElement}
 import play.api.libs.json.{JsValue, Json}
-import uk.gov.hmrc.decisionservice.model.api.{DecisionRequest, Fact, FactSection}
+import uk.gov.hmrc.decisionservice.model.api.{Section, QuestionSet}
 import uk.gov.hmrc.play.test.UnitSpec
 
 class DecisionRequestSpec extends UnitSpec with BeforeAndAfterEach with ScalaFutures with LoneElement with Inspectors with IntegrationPatience {
@@ -14,16 +14,11 @@ class DecisionRequestSpec extends UnitSpec with BeforeAndAfterEach with ScalaFut
       |  "version" : "1.0",
       |  "sections" : [ {
       |    "name" : "personal-service",
-      |    "questions" : [ {
-      |      "name" : "1",
-      |      "answer" : true
-      |    }, {
-      |      "name" : "2",
-      |      "answer" : false
-      |    }, {
-      |      "name" : "3",
-      |      "answer" : true
-      |    } ]
+      |    "facts" : {
+      |      "1" : true,
+      |      "2" : false,
+      |      "3" : true
+      |    }
       |  } ]
       |}
       |
@@ -32,13 +27,13 @@ class DecisionRequestSpec extends UnitSpec with BeforeAndAfterEach with ScalaFut
   "decision request json" should {
     "be correctly converted to Scala object" in {
       val parsed = Json.parse(json)
-      val jsResult = Json.fromJson[DecisionRequest](parsed)
+      val jsResult = Json.fromJson[QuestionSet](parsed)
       jsResult.isSuccess shouldBe true
       val obj = jsResult.get
       obj.sections should have size 1
-      obj.sections(0).questions should have size 3
+      obj.sections(0).facts should have size 3
 
-      val m = obj.sections(0).questions.map(a => (a.name -> a.answer)).toMap
+      val m:Map[String,Boolean] = obj.sections(0).facts
       val res = (1 to 3).flatMap(i => m.get(i.toString))
       res should contain theSameElementsInOrderAs (List(true, false, true))
     }
@@ -46,20 +41,20 @@ class DecisionRequestSpec extends UnitSpec with BeforeAndAfterEach with ScalaFut
 
   "decision request Scala object" should {
     "be correctly converted to json object" in {
-      val personalServiceQuestions = List(Fact("1", true),  Fact("2", false), Fact("3", true))
-      val helperQuestions = List(Fact("1", false), Fact("2", false), Fact("3", false))
-      val controlQuestions = List(Fact("1", true),  Fact("2", true),  Fact("3", true))
+      val personalServiceQuestions = Map("1" -> true, "2" -> false, "3" -> true)
+      val helperQuestions = Map("1" -> false, "2" -> false, "3" -> false)
+      val controlQuestions = Map("1" -> true, "2" -> true, "3" -> true)
       val sections = List(
-        FactSection("personal-service", personalServiceQuestions),
-        FactSection("helper", helperQuestions),
-        FactSection("control", controlQuestions)
+        Section("personal-service", personalServiceQuestions),
+        Section("helper", helperQuestions),
+        Section("control", controlQuestions)
       )
-      val decisionRequest = DecisionRequest("1.0", sections)
+      val decisionRequest = QuestionSet("1.0", sections)
       val jsValue:JsValue = Json.toJson(decisionRequest)
       val jsections = jsValue \\ "sections"
-      val jquestions = jsValue \\ "questions"
+      val jfacts = jsValue \\ "facts"
       jsections should have size 1
-      jquestions should have size 3
+      jfacts should have size 3
     }
   }
 
