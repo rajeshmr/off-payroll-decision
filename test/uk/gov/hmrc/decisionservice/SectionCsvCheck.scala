2b50a185df7c447860882e57d3de4806b44229b7
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2016-11-16 20:09:31 +0000

    removed duplication of headings in csv tests

--- test/uk/gov/hmrc/decisionservice/SectionCsvCheck.scala
+++ test/uk/gov/hmrc/decisionservice/SectionCsvCheck.scala
@@ -1,43 +1,44 @@
 package uk.gov.hmrc.decisionservice
 import cats.data.Xor
 import org.scalacheck.{Gen, Prop, Properties}
-import uk.gov.hmrc.decisionservice.model.RulesFileLoadError
 import uk.gov.hmrc.decisionservice.model.rules.SectionRuleSet
 import uk.gov.hmrc.decisionservice.ruleengine.{RulesFileMetaData, SectionFactMatcher, SectionRulesLoader}
 import uk.gov.hmrc.play.test.UnitSpec
 
 
 trait CsvCheck {
-  def prettyPrint(m:Map[String,String]):Unit = print(m.keySet.toList.sorted.map(m(_)).mkString("\t"))
-  def check(m:Map[String,String],maybeRules:Xor[RulesFileLoadError,SectionRuleSet]) = {
+  def prettyPrint(m: Map[String, String]): Unit = print(m.keySet.toList.sorted.map(a=>s"${a} ${m(a)}").mkString("\t"))
+
+  def check(l: List[String], ruleSet: SectionRuleSet):Boolean = {
     println
+    val pairs = ruleSet.headings zip l
+    val m = Map(pairs: _*)
     prettyPrint(m)
-    maybeRules.map { ruleSet =>
-      val response = SectionFactMatcher.matchFacts(m, ruleSet)
-      response match {
-        case Xor.Right(sectionResult) =>
-          print(s"\t${sectionResult.value}")
-        case Xor.Left(e) =>
-          print(s"\t$e")
-      }
+    val response = SectionFactMatcher.matchFacts(m, ruleSet)
+    response match {
+      case Xor.Right(sectionResult) =>
+        print(s"\t${sectionResult.value}")
+      case Xor.Left(e) =>
+        print(s"\t$e")
     }
+    true
   }
 }
-
 
 object BusinessStructureCheck extends Properties("business structure check") with CsvCheck with UnitSpec {
   val csvMetadata = RulesFileMetaData(7, 2, "/business_structure.csv")
   val gen = for {
     y <- Gen.listOfN(csvMetadata.valueCols, Gen.oneOf[String]("Yes", "No"))
   } yield {
-    Map("8a" -> y(0), "8b" -> y(1), "8c" -> y(2), "8d" -> y(3), "8e" -> y(4), "8f" -> y(5), "8g" -> y(6))
+    y
   }
   val maybeRules = SectionRulesLoader.load(csvMetadata)
-  property("rule should match all possible facts") =
-    Prop.forAll(gen) { m =>
-      check(m,maybeRules)
-      true
-    }
+  maybeRules.map { ruleSet =>
+    property("rule should match all possible facts") =
+      Prop.forAll(gen) { l =>
+        check(l, ruleSet)
+      }
+  }
 }
 
 
@@ -46,12 +47,13 @@ object PersonalServiceCheck extends Properties("personal service check") with Cs
   val gen = for {
     y <- Gen.listOfN(csvMetadata.valueCols, Gen.oneOf[String]("Yes", "No"))
   } yield {
-    Map("2" -> y(0), "3" -> y(1), "4" -> y(2), "5" -> y(3), "6" -> y(4), "7" -> y(5), "8" -> y(6), "9" -> y(7), "10" -> y(8))
+    y
   }
   val maybeRules = SectionRulesLoader.load(csvMetadata)
-  property("rule should match all possible facts") =
-    Prop.forAll(gen) { m =>
-      check(m,maybeRules)
-      true
-    }
+  maybeRules.map { ruleSet =>
+    property("rule should match all possible facts") =
+      Prop.forAll(gen) { l =>
+        check(l, ruleSet)
+      }
+  }
 }
