0698d57ca07b61f04a954af76c11368d993f9d53
REDACTED <REDACTED>
2016-11-17 13:20:57 +0000

    changed format of questonSet to map of maps

--- app/uk/gov/hmrc/decisionservice/model/api/ApiModel.scala
+++ app/uk/gov/hmrc/decisionservice/model/api/ApiModel.scala
@@ -3,13 +3,7 @@ package uk.gov.hmrc.decisionservice.model.api
 import play.api.libs.json.{Format, Json}
 
 
-case class Section(name:String, facts:Map[String,Boolean])
-
-case class QuestionSet(version:String, sections:List[Section])
-
-object Section {
-  implicit val sectionFormat: Format[Section] = Json.format[Section]
-}
+case class QuestionSet(version:String, sections:Map[String,Map[String,Boolean]])
 
 object QuestionSet {
   implicit val questionSetFormat: Format[QuestionSet] = Json.format[QuestionSet]
