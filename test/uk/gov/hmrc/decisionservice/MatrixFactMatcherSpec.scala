869d5911347f3fb380b6b91339ea464ade940f4f
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2016-11-18 18:17:46 +0000

    added support for exit carry-overs

--- test/uk/gov/hmrc/decisionservice/MatrixFactMatcherSpec.scala
+++ test/uk/gov/hmrc/decisionservice/MatrixFactMatcherSpec.scala
@@ -11,12 +11,12 @@ class MatrixFactMatcherSpec extends UnitSpec with BeforeAndAfterEach with ScalaF
   "matrix fact matcher" should {
     "produce correct result for a sample matrix fact" in {
       val matrixFacts = Map(
-        "BusinessStructure" -> CarryOverImpl("high", true), "Substitute" -> CarryOverImpl("high" , false)
+        "BusinessStructure" -> CarryOverImpl("high", false), "Substitute" -> CarryOverImpl("high" , false)
       )
       val matrixRules = List(
-        MatrixRule(List(CarryOverImpl("high"  , true ),CarryOverImpl("low" , true )), DecisionInIR35),
-        MatrixRule(List(CarryOverImpl("high"  , true ),CarryOverImpl("high", false)), DecisionOutOfIR35),
-        MatrixRule(List(CarryOverImpl("medium", true ),CarryOverImpl("high", true )), DecisionInIR35)
+        MatrixRule(List(CarryOverImpl("high"  , false ),CarryOverImpl("low" , false )), DecisionInIR35),
+        MatrixRule(List(CarryOverImpl("high"  , false ),CarryOverImpl("high", false)), DecisionOutOfIR35),
+        MatrixRule(List(CarryOverImpl("medium", false ),CarryOverImpl("high", false )), DecisionInIR35)
       )
       val matrixRuleSet = MatrixRuleSet(List("BusinessStructure", "Substitute"), matrixRules)
 
@@ -29,14 +29,14 @@ class MatrixFactMatcherSpec extends UnitSpec with BeforeAndAfterEach with ScalaF
     }
     "produce correct result for a partial fact" in {
       val matrixFacts = Map(
-        "BusinessStructure" -> CarryOverImpl("high", true),
+        "BusinessStructure" -> CarryOverImpl("high", false),
         "Substitute" -> CarryOverImpl("low" , false),
         "FinancialRisk" -> CarryOverImpl("" , false)
       )
       val matrixRules = List(
-        MatrixRule(List(CarryOverImpl("high"  , true ),CarryOverImpl("high" , true ),CarryOverImpl("" , true )), DecisionInIR35),
-        MatrixRule(List(CarryOverImpl("high"  , true ),CarryOverImpl("low" , false),CarryOverImpl("" , true )), DecisionInIR35),
-        MatrixRule(List(CarryOverImpl("medium", true ),CarryOverImpl("high", true ),CarryOverImpl("low" , true )), DecisionOutOfIR35)
+        MatrixRule(List(CarryOverImpl("high"  , false ),CarryOverImpl("high" , false ),CarryOverImpl("" , false )), DecisionInIR35),
+        MatrixRule(List(CarryOverImpl("high"  , false ),CarryOverImpl("low" , false),CarryOverImpl("" , false )), DecisionInIR35),
+        MatrixRule(List(CarryOverImpl("medium", false ),CarryOverImpl("high", false ),CarryOverImpl("low" , false )), DecisionOutOfIR35)
       )
       val matrixRuleSet = MatrixRuleSet(List("BusinessStructure", "Substitute", "FinancialRisk"), matrixRules)
 
@@ -44,6 +44,25 @@ class MatrixFactMatcherSpec extends UnitSpec with BeforeAndAfterEach with ScalaF
       response.isRight shouldBe true
       response.map { decision =>
         decision shouldBe DecisionInIR35
+      }
+    }
+    "produce correct result for exit carry over" in {
+      val matrixFacts = Map(
+        "BusinessStructure" -> CarryOverImpl("high", false),
+        "Substitute" -> CarryOverImpl("low" , false),
+        "FinancialRisk" -> CarryOverImpl("out of IR35" , true)
+      )
+      val matrixRules = List(
+        MatrixRule(List(CarryOverImpl("high"  , false ),CarryOverImpl("high" , false ),CarryOverImpl("" , false )), DecisionInIR35),
+        MatrixRule(List(CarryOverImpl("high"  , false ),CarryOverImpl("low" , false),CarryOverImpl("" , false )), DecisionInIR35),
+        MatrixRule(List(CarryOverImpl("medium", false ),CarryOverImpl("high", false ),CarryOverImpl("low" , false )), DecisionOutOfIR35)
+      )
+      val matrixRuleSet = MatrixRuleSet(List("BusinessStructure", "Substitute", "FinancialRisk"), matrixRules)
+
+      val response = MatrixFactMatcher.matchFacts(matrixFacts, matrixRuleSet)
+      response.isRight shouldBe true
+      response.map { decision =>
+        decision shouldBe MatrixDecisionImpl("out of IR35")
       }
     }
   }
