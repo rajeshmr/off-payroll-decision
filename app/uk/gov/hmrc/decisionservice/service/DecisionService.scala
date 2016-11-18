3f75475c1157756994d4fd5639ede15f6231b827
REDACTED <REDACTED>
2016-11-18 14:49:28 +0000

    intermediate decision service impl continued

--- app/uk/gov/hmrc/decisionservice/service/DecisionService.scala
+++ app/uk/gov/hmrc/decisionservice/service/DecisionService.scala
@@ -1,74 +1,63 @@
 package uk.gov.hmrc.decisionservice.service
 
 import cats.data.Xor
-import uk.gov.hmrc.decisionservice.model.{DecisionServiceError, RulesFileLoadError}
+import uk.gov.hmrc.decisionservice.model.{DecisionServiceError, FactError, RulesFileLoadError}
 import uk.gov.hmrc.decisionservice.model.api.QuestionSet
 import uk.gov.hmrc.decisionservice.model.rules._
-import uk.gov.hmrc.decisionservice.ruleengine.{MatrixRulesLoader, RulesFileMetaData, SectionFactMatcher, SectionRulesLoader}
+import uk.gov.hmrc.decisionservice.ruleengine._
 
 
 trait DecisionService {
-
   def evaluate(questionSet:QuestionSet, matrixRuleSet:MatrixRuleSet):Xor[DecisionServiceError,MatrixDecision]
-
 }
 
 
-object DecisionServiceInstance extends App /*DecisionService*/ {
+object DecisionServiceInstance extends DecisionService {
   val csvSectionMetadata = List(
     (7, 2, "/business_structure.csv", "BusinessStructure"),
     (9, 2, "/personal_service.csv", "PersonalService")
   ).collect{case (q,r,f,n) => RulesFileMetaData(q,r,f,n)}
   val csvMatrixMetadata = RulesFileMetaData(2, 2, "matrix.csv", "matrix")
 
-
-  def loadSectionRules():Xor[List[RulesFileLoadError],List[SectionRuleSet]] = {
+  def loadSectionRules():Xor[DecisionServiceError,List[SectionRuleSet]] = {
     val maybeRules = csvSectionMetadata.map(SectionRulesLoader.load(_))
     val rulesErrors = maybeRules.map(_.fold(e => Some(e),r => None)).flatten
     val rules = maybeRules.collect{case Xor.Right(x) => x}
-    if (rulesErrors.isEmpty) Xor.right(rules) else Xor.left(rulesErrors)
+    if (rulesErrors.isEmpty) Xor.right(rules) else Xor.left(rulesErrors.head) // TODO aggregate multiple errors here
   }
 
-  def loadMatrixRules():Xor[RulesFileLoadError,MatrixRuleSet] = {  // senza sensible :-)
+  def loadMatrixRules():Xor[DecisionServiceError,MatrixRuleSet] = {
     val maybeRules = MatrixRulesLoader.load(csvMatrixMetadata)
     maybeRules
   }
 
   def evaluate(questionSet:QuestionSet, matrixRuleSet:MatrixRuleSet):Xor[DecisionServiceError,MatrixDecision] = {
-
-//    loadSectionRules() match {
-//      case Xor.Left(e) => Xor.left(e.head) // TODO aggregate multiple errors here
-//      case Xor.Right(sectionRules) =>
-//        loadMatrixRules() match {
-//          case Xor.Left(e) =>  Xor.left(e)
-//          case Xor.Right(matrixRule) =>
-//
-//        }
-//    }
-
-    Xor.right(DecisionInIR35)
-  }
-
-
-  def applyToSectionRules(questionSet:QuestionSet,sectionRules:List[SectionRuleSet]):Xor[DecisionServiceError,List[CarryOver]] = {
-
-    sectionRules.map { sectionRules =>
-      val sectionName = sectionRules.section
-      val maybeRule = questionSet.sections.get(sectionName)
-      maybeRule.map { rule =>
-        val result = SectionFactMatcher.matchFacts(rule, sectionRules)
-
-      }
+    val maybeDecision = for {
+      sectionRules <- loadSectionRules()
+      matrixRules <- loadMatrixRules()
+      carryOvers <- applyToSectionRules(questionSet, sectionRules)
+      decision <- applyToMatrixRules(carryOvers, matrixRules)
+    }
+    yield {
+      decision
     }
 
-//    }
-//
-//    questionSet.sections.keys.map{ sectionName =>
-
-    Xor.left(RulesFileLoadError(""))
+    maybeDecision
   }
 
-  println(loadSectionRules())
+  def applyToSectionRules(questionSet:QuestionSet,sectionRules:List[SectionRuleSet]):Xor[DecisionServiceError,Map[String,CarryOver]] = {
+    val pairs = for {
+      sectionRuleSet <- sectionRules
+      sectionFacts <- questionSet.sections.get(sectionRuleSet.section)
+    } yield {
+      (sectionRuleSet.section, SectionFactMatcher.matchFacts(sectionFacts, sectionRuleSet))
+    }
+    val rulesErrors = pairs.map(_._2.fold(e => Some(e),r => None)).flatten
+    val entries = pairs.collect{case (sectionName,Xor.Right(x)) => (sectionName,x)}
+    if (rulesErrors.isEmpty) Xor.right(Map(entries: _*)) else Xor.left(rulesErrors.head) // TODO aggregate multiple errors here
+  }
 
+  def applyToMatrixRules(carryOvers:Map[String,CarryOver], matrixRules:MatrixRuleSet):Xor[DecisionServiceError,MatrixDecision] =
+    MatrixFactMatcher.matchFacts(carryOvers, matrixRules)
 
 }
