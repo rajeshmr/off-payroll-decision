6e8de567b51fc14adabedc3a945784e689c6cd24
REDACTED <REDACTED>
2016-11-21 13:47:06 +0000

    added rudimentary csv file validation

--- test/uk/gov/hmrc/decisionservice/DecisionServiceErrorSpec.scala
+++ test/uk/gov/hmrc/decisionservice/DecisionServiceErrorSpec.scala
@@ -7,34 +7,51 @@ import uk.gov.hmrc.play.test.UnitSpec
 
 class DecisionServiceErrorSpec extends UnitSpec {
 
-  object DecisionServiceTestInstance extends DecisionService {
+  object DecisionServiceNotExistingCsvTestInstance extends DecisionService {
     lazy val maybeSectionRules = loadSectionRules()
     lazy val maybeMatrixRules = loadMatrixRules()
 
     val csvSectionMetadata = List(
-      (7, 2, "business_structure.csv", "BusinessStructure"),
-      (9, 2, "personal_service.csv", "PersonalService")
+      (7, 2, "business_structure_not_existing.csv", "BusinessStructure"),
+      (9, 2, "personal_service_not_existing.csv", "PersonalService")
     ).collect{case (q,r,f,n) => RulesFileMetaData(q,r,f,n)}
-
-    val csvMatrixMetadata = RulesFileMetaData(2, 1, "matrix.csv", "matrix")
+    val csvMatrixMetadata = RulesFileMetaData(2, 1, "/matrix.csv", "matrix")
   }
+
+  object DecisionServiceCsvWithErrorsTestInstance extends DecisionService {
+    lazy val maybeSectionRules = loadSectionRules()
+    lazy val maybeMatrixRules = loadMatrixRules()
+
+    val csvSectionMetadata = List(
+      (7, 2, "/business_structure_errors.csv", "BusinessStructure"),
+      (9, 2, "/personal_service_errors.csv", "PersonalService")
+    ).collect{case (q,r,f,n) => RulesFileMetaData(q,r,f,n)}
+    val csvMatrixMetadata = RulesFileMetaData(2, 1, "/matrix.csv", "matrix")
+  }
+
+  val facts =
+    Map(
+      "BusinessStructure" -> Map("8a" -> "yes", "8g" -> "no"),
+      "PersonalService" -> Map("2" -> "yes", "10" -> "yes")
+    )
+  val questionSet = QuestionSet("1.0", facts)
 
 
   "decision service with initialization error" should {
     "correctly report multiple aggregated error information" in {
-      val facts =
-      Map(
-        "BusinessStructure" -> Map("8a" -> "yes", "8g" -> "no"),
-        "PersonalService" -> Map("2" -> "yes", "10" -> "yes")
-      )
-      val questionSet = QuestionSet("1.0", facts)
-
-      val maybeDecision = DecisionServiceTestInstance.evaluate(questionSet)
-
+      val maybeDecision = DecisionServiceNotExistingCsvTestInstance.evaluate(questionSet)
       maybeDecision.isLeft shouldBe true
       maybeDecision.leftMap { error =>
-        error.message.contains("business_structure.csv") shouldBe true
-        error.message.contains("personal_service.csv") shouldBe true
+        error.message.contains("business_structure_not_existing.csv") shouldBe true
+        error.message.contains("personal_service_not_existing.csv") shouldBe true
+      }
+    }
+    "correctly report errors in csv files" in {
+      val maybeDecision = DecisionServiceCsvWithErrorsTestInstance.evaluate(questionSet)
+      println(maybeDecision)
+      maybeDecision.isLeft shouldBe true
+      maybeDecision.leftMap { error =>
+        error.message.contains("Yes, Yes, Yes, Yes, Yes, No, No, High, false, a") shouldBe true // TODO rework this - we need more detailed info here
       }
     }
   }
