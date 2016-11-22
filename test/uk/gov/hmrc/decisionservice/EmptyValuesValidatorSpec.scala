ee5b2e147d5684bc076992aaa948916ecab673d0
REDACTED <REDACTED>
2016-11-22 14:47:50 +0000

    converted tests to new model and rule engine

--- test/uk/gov/hmrc/decisionservice/EmptyValuesValidatorSpec.scala
+++ test/uk/gov/hmrc/decisionservice/EmptyValuesValidatorSpec.scala
@@ -9,50 +9,45 @@ import uk.gov.hmrc.play.test.UnitSpec
 
 class EmptyValuesValidatorSpec extends UnitSpec with BeforeAndAfterEach with ScalaFutures with LoneElement with Inspectors with IntegrationPatience {
 
-//  object SectionEmptyValuesValidator extends EmptyValuesValidator {
-//    type ValueType = String
-//    type Rule = SectionRule
-//    type RuleResult = CarryOver
+  object SectionEmptyValuesValidator extends EmptyValuesValidator {
+    type RuleResult = CarryOver
 
-//    def valueEmpty(s: String) = s.isEmpty
-//    def notValidUseCase: CarryOver = SectionNotValidUseCase
-//  }
+    def valueEmpty(carryOver: CarryOver) = carryOver.value.isEmpty
+    def notValidUseCase: CarryOver = SectionNotValidUseCase
+  }
 
-//  "empty values validator" should {
-//    "produce fact error if FactsEmptySet is a subset of MaximumRulesEmptySet" in {
-//      val fact = Map(
-//        ("question1" -> "yes"),
-//        ("question2" -> ""),
-//        ("question3" -> ""))
-//      val rules = List(
-//        SectionRule(List("yes","yes","yes"), CarryOverImpl("high"  , true)),
-//        SectionRule(List("yes","no" ,"no" ), CarryOverImpl("medium", true)),
-//        SectionRule(List("no" ,"yes",""   ), CarryOverImpl("low"   , false))
-//      )
-//
-//      val error = SectionEmptyValuesValidator.noMatchResult(fact,rules)
-//      error.isLeft should be (true)
-//      error.leftMap { e =>
-//        e shouldBe a [FactError]
-//      }
-//    }
-//    "produce 'not valid use case' result if FactsEmptySet is a superset of MaximumRulesEmptySet" in {
-//      val fact = Map(
-//        ("question1" -> "yes"),
-//        ("question2" -> ""),
-//        ("question3" -> ""))
-//      val rules = List(
-//        SectionRule(List("yes","yes","yes"), CarryOverImpl("high"  , true)),
-//        SectionRule(List("yes","no" ,""   ), CarryOverImpl("medium", true)),
-//        SectionRule(List("no" ,""   ,""   ), CarryOverImpl("low"   , false))
-//      )
-//
-//      val result = SectionEmptyValuesValidator.noMatchResult(fact,rules)
-//      result.isRight should be (true)
-//      result.map { r =>
-//        r shouldBe SectionNotValidUseCase
-//      }
-//    }
-//  }
+  val facts = Facts(Map(
+    ("question1" -> >>>("yes")),
+    ("question2" -> >>>("")),
+    ("question3" -> >>>(""))))
+
+  "empty values validator" should {
+    "produce fact error if FactsEmptySet is a subset of MaximumRulesEmptySet" in {
+      val rules = List(
+        SectionRule(List(>>>("yes"), >>>("yes"), >>>("yes")), >>>("high"  , true)),
+        SectionRule(List(>>>("yes"), >>>("no" ), >>>("no" )), >>>("medium", true)),
+        SectionRule(List(>>>("no" ), >>>("yes"), >>>(""   )), >>>("low"         ))
+      )
+
+      val error = SectionEmptyValuesValidator.noMatchResult(facts.facts,rules)
+      error.isLeft should be (true)
+      error.leftMap { e =>
+        e shouldBe a [FactError]
+      }
+    }
+    "produce 'not valid use case' result if FactsEmptySet is a superset of MaximumRulesEmptySet" in {
+      val rules = List(
+        SectionRule(List(>>>("yes"), >>>("yes"), >>>("yes")), >>>("high"  , true)),
+        SectionRule(List(>>>("yes"), >>>("no" ), >>>(""   )), >>>("medium", true)),
+        SectionRule(List(>>>("no" ), >>>(""   ), >>>(""   )), >>>("low"         ))
+      )
+
+      val result = SectionEmptyValuesValidator.noMatchResult(facts.facts,rules)
+      result.isRight should be (true)
+      result.map { r =>
+        r shouldBe SectionNotValidUseCase
+      }
+    }
+  }
 
 }
