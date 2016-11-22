91115db138213d31c860647358730fd41dd73355
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2016-11-22 13:35:17 +0000

    proper test for rule engine

---
+++ test/uk/gov/hmrc/decisionservice/RuleEngineSpec.scala
@@ -0,0 +1,64 @@
+package uk.gov.hmrc.decisionservice
+import uk.gov.hmrc.decisionservice.model.rules._
+import uk.gov.hmrc.decisionservice.ruleengine.{RuleEngine, RuleEngineUndecided}
+import uk.gov.hmrc.play.test.UnitSpec
+
+class RuleEngineSpec extends UnitSpec {
+  val sectionRules1 = List(
+    SectionRule(List(>>>("yes"), >>>("yes"), >>>("yes")),   >>>("high"  , true)),
+    SectionRule(List(>>>("yes"), >>>("no") , >>>("no")) ,   >>>("medium", true)),
+    SectionRule(List(>>>("yes"), >>>("no") , >>>("yes")),   >>>("low"   , false)),
+    SectionRule(List(>>>("yes"), >>>("")   , >>>("yes")),   >>>("low"   ))
+  )
+  val sectionRules2 = List(
+    SectionRule(List(>>>("no"), >>>("low"   ), >>>("yes")),   >>>("medium2"  , true)),
+    SectionRule(List(>>>("no"), >>>("medium"), >>>("yes")),   >>>("high2"    , true)),
+    SectionRule(List(>>>("no"), >>>("high"  ), >>>("yes")),   >>>("low2"     , true))
+  )
+  val ruleSet1 = SectionRuleSet("sectionName1", List("question1", "question2", "question3"), sectionRules1)
+  val ruleSet2 = SectionRuleSet("sectionName2", List("question4", "sectionName1", "question6"), sectionRules2)
+
+  val rules = Rules(List(ruleSet1,ruleSet2))
+
+  "rule engine" should {
+    "produce correct decision for a sample fact and rules with a simple inference" in {
+      val facts = Facts(Map(
+        "question1" -> >>>("yes"),
+        "question2" -> >>>("no" ),
+        "question3" -> >>>("yes"),
+        "question4" -> >>>("no" ),
+        "question6" -> >>>("yes")))
+      val maybeDecision = RuleEngine.processRules(rules, facts)
+      maybeDecision.isRight shouldBe true
+      maybeDecision.map { decision =>
+        decision.value shouldBe "medium2"
+      }
+    }
+    "produce correct decision for a sample fact and rules with a short-circuit exit" in {
+      val facts = Facts(Map(
+        "question1" -> >>>("yes"),
+        "question2" -> >>>("yes"),
+        "question3" -> >>>("yes"),
+        "question4" -> >>>("no" ),
+        "question6" -> >>>("yes")))
+      val maybeDecision = RuleEngine.processRules(rules, facts)
+      maybeDecision.isRight shouldBe true
+      maybeDecision.map { decision =>
+        decision.value shouldBe "high"
+      }
+    }
+    "produce undecided for facts leading to no decision" in {
+      val facts = Facts(Map(
+        "question1" -> >>>("yes"),
+        "question2" -> >>>("no" ),
+        "question3" -> >>>("yes"),
+        "question4" -> >>>("yes"),
+        "question6" -> >>>("yes")))
+      val maybeDecision = RuleEngine.processRules(rules, facts)
+      maybeDecision.isRight shouldBe true
+      maybeDecision.map { decision =>
+        decision shouldBe RuleEngineUndecided
+      }
+    }
+  }
+}
