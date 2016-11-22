3e5e7237371e125a13b442a94f5391a4d9b33b61
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2016-11-22 14:24:39 +0000

    decision service error spec with new engine

--- app/uk/gov/hmrc/decisionservice/service/DecisionService.scala
+++ app/uk/gov/hmrc/decisionservice/service/DecisionService.scala
@@ -1,14 +1,12 @@
 package uk.gov.hmrc.decisionservice.service
 
 import cats.data.Xor
-import uk.gov.hmrc.decisionservice.model.{DecisionServiceError, FactError, RulesFileLoadError}
-import uk.gov.hmrc.decisionservice.model.api.QuestionSet
+import uk.gov.hmrc.decisionservice.model.{DecisionServiceError, RulesFileLoadError}
 import uk.gov.hmrc.decisionservice.model.rules._
 import uk.gov.hmrc.decisionservice.ruleengine._
 
 
 trait DecisionService {
-
   type CarryOverFacts = Map[String,CarryOver]
 
   val maybeSectionRules:Xor[DecisionServiceError,List[SectionRuleSet]]
@@ -25,40 +23,13 @@ trait DecisionService {
     }
   }
 
-//  def >>>:(questionSet:QuestionSet):Xor[DecisionServiceError,RuleEngineDecision] = {
-//    val maybeDecision = for {
-//      sectionRules <- maybeSectionRules
-//      carryOvers <- applyToSectionRules(questionSet, sectionRules)
-//    }
-//    yield {
-//      decision
-//    }
-//    maybeDecision
-//  }
-
-  def >>>:(facts:Facts):Xor[DecisionServiceError,RuleEngineDecision] = {
+  def ==>:(facts:Facts):Xor[DecisionServiceError,RuleEngineDecision] = {
     maybeSectionRules match {
       case Xor.Right(sectionRules) =>
         RuleEngine.processRules(Rules(sectionRules),facts)
       case ee@Xor.Left(_) => ee
     }
   }
-
-//  private def applyToSectionRules(questionSet:QuestionSet,sectionRules:List[SectionRuleSet]):Xor[DecisionServiceError,CarryOverFacts] = {
-//    val pairs = for {
-//      sectionRuleSet <- sectionRules
-//      sectionFacts <- questionSet.sections.get(sectionRuleSet.section)
-//    } yield {
-//      (sectionRuleSet.section, SectionFactMatcher.matchFacts(sectionFacts, sectionRuleSet))
-//    }
-//    val rulesErrors = pairs.collect {case (a,Xor.Left(x)) => x}
-//    val entries = pairs.collect {case (sectionName,Xor.Right(x)) => (sectionName,x)}
-//    rulesErrors match {
-//      case Nil => Xor.right(Map(entries: _*))
-//      case _ => Xor.left(rulesErrors.head)
-//    }
-//  }
-
 }
 
 
