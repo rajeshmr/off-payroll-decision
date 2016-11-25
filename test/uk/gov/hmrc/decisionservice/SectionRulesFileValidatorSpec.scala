375bde7a6ee9dad6bfc888f42f4ff8895436b4dd
REDACTED <REDACTED>
2016-11-25 11:13:12 +0000

    refactored test for rules file validator

--- test/uk/gov/hmrc/decisionservice/SectionRulesFileValidatorSpec.scala
+++ test/uk/gov/hmrc/decisionservice/SectionRulesFileValidatorSpec.scala
@@ -8,132 +8,73 @@ import uk.gov.hmrc.decisionservice.ruleengine.SectionRuleValidator._
 
 class SectionRulesFileValidatorSpec extends UnitSpec {
 
-  object RowFixture {
-    private val headers = List("Section1", "Section2", "Section3", "Section4")
-    private val result = List("Decision")
-    private val validAnswers = List("Low", "Medium", "", "High")
-    private val validAnswers_result = List("In IR35")
-    private val invalidAnswers = List("medium", "Bob", "low", "")
-    private val invalidAnswers_decision = List("whatever")
-    val validHeaderRow = headers ::: result
-    val validRuleRow = validAnswers ::: validAnswers_result
-    val ruleRowWithInvalidRuleText = invalidAnswers ::: validAnswers_result
-    val ruleRowWithInvalidDecision = validAnswers ::: invalidAnswers_decision
-  }
+  val valueHeaders = List("Q1", "Q2", "Q3", "Q4")
+  val resultHeaders = List("CarryOver", "Exit")
 
-  object RowFixture2 {
-    val headers = List("Q1", "Q2", "Q3", "Q4")
-    val resultPair = List("CarryOver", "Exit")
-    val headerRow = headers ::: resultPair
+  val validRuleValues1 = List("Low", "Medium", "", "High")
+  val validRuleValues2 = List("Yes", "No", "Yes", "")
+  val invalidRuleValues = List("Yes", "Bob", "Yes", "")
 
-    val validAnswers = List("Yes", "No", "Yes", "")
-    val validAnswers_resultPair = List("Low", "false")
-
-    val validRuleRow = validAnswers ::: validAnswers_resultPair
-
-    val invalidAnswers = List("Yes", "Bob", "Yes", "")
-    val ruleRowWithInvalidRuleText = invalidAnswers ::: validAnswers_resultPair
-
-    val invalidAnswers_carryOver = List("whatever", "true")
-    val ruleRowWithInvalidCarryOverText = validAnswers ::: invalidAnswers_carryOver
-
-    val invalidAnswers_exit = List("low", "wrong!!!")
-    val ruleRowWithInvalidExitText = validAnswers ::: invalidAnswers_exit
-  }
-
-  object MetadataFixture {
-    val valid = RulesFileMetaData(4, 1, "", "")
-    val headerSizeMismatch = RulesFileMetaData(9, 1, "", "")
-    val validAnswer = RulesFileMetaData(4, 1, "", "")
-    val answerSizeMismatch = RulesFileMetaData(41, 1, "", "")
-    val invalidAnswer = RulesFileMetaData(4, 1, "", "")
-    val invalidCarryOver = RulesFileMetaData(4, 1, "", "")
-  }
-
-  object MetadataFixture2 {
-    val validHeaderMetaData: RulesFileMetaData = RulesFileMetaData(4, 2, "", "")
-    val metaData_HeaderSizeMismatch: RulesFileMetaData = RulesFileMetaData(9, 2, "", "")
-    val validAnswerMetaData: RulesFileMetaData = RulesFileMetaData(4, 2, "", "")
-    val metaData_withInvalidAnswer: RulesFileMetaData = RulesFileMetaData(4, 2, "", "")
-    val metaData_withInvalidCarryOver: RulesFileMetaData = RulesFileMetaData(4, 2, "", "")
-    val metaData_withInvalidExit: RulesFileMetaData = RulesFileMetaData(4, 2, "", "")
-
-  }
-
+  val validNonExitResultColumns1 = List("In IR35")
+  val validNonExitResultColumns2 = List("Low", "false")
+  val invalidExitResultColumns = List("whatever", "true")
+  val invalidResultColumns = List("low", "wrong!!!")
 
   "section rules file validator" should {
 
     "validate correct column header size" in {
-      val mayBeValid = validateColumnHeaders(RowFixture.validHeaderRow, MetadataFixture.valid)
+      val (v,r) = (valueHeaders,resultHeaders)
+      val mayBeValid = validateColumnHeaders(v ::: r, RulesFileMetaData(v.size, r.size, "", ""))
       mayBeValid.isRight shouldBe true
     }
-
-    "validate correct column header size (2)" in {
-      val mayBeValid = validateColumnHeaders(RowFixture2.headerRow, MetadataFixture2.validHeaderMetaData)
-      mayBeValid.isRight shouldBe true
-    }
-
     "return error for invalid column header size" in {
-      val mayBeValid = validateColumnHeaders(RowFixture.validHeaderRow, MetadataFixture.headerSizeMismatch)
+      val (v,r) = (valueHeaders,resultHeaders)
+      val mayBeValid = validateColumnHeaders(v ::: r, RulesFileMetaData(v.size, r.size+1, "", ""))
       mayBeValid.isLeft shouldBe true
       mayBeValid.leftMap { error =>
         error shouldBe a[RulesFileError]
-        error.message shouldBe "Column header size does not match metadata"
       }
     }
-
-    "return error for invalid column header size (2)" in {
-      val mayBeValid = validateColumnHeaders(RowFixture2.headerRow, MetadataFixture2.metaData_HeaderSizeMismatch)
-      mayBeValid.isLeft shouldBe true
-      mayBeValid.leftMap { error =>
-        error shouldBe a[RulesFileError]
-        error.message shouldBe "Column header size does not match metadata"
-      }
-    }
-
     "return error for rule row size mismatch" in {
-      val mayBeValid = validateRuleRow(RowFixture.validRuleRow, MetadataFixture.answerSizeMismatch, 3)
+      val (v,r) = (validRuleValues1,validNonExitResultColumns1)
+      val mayBeValid = validateRuleRow(v ::: r, RulesFileMetaData(41, 1, "", ""), 3)
       mayBeValid.isLeft shouldBe true
       mayBeValid.leftMap { error =>
         error shouldBe a[RulesFileError]
         error.message shouldBe "Row size does not match metadata on row 3"
       }
     }
-
     "correctly validate valid rule row" in {
-      val mayBeValid = validateRuleRow(RowFixture2.validRuleRow, MetadataFixture2.validAnswerMetaData, 3)
+      val (v,r) = (validRuleValues2,validNonExitResultColumns2)
+      val mayBeValid = validateRuleRow(v ::: r, RulesFileMetaData(v.size, r.size, "", ""), 3)
       mayBeValid.isRight shouldBe true
-
     }
-
     "return error for invalid rule text" in {
-      val mayBeValid = validateRuleRow(RowFixture2.ruleRowWithInvalidRuleText, MetadataFixture2.metaData_withInvalidAnswer, 4)
+      val (v,r) = (invalidRuleValues,validNonExitResultColumns2)
+      val mayBeValid = validateRuleRow(v ::: r, RulesFileMetaData(v.size, r.size, "", ""), 4)
       mayBeValid.isLeft shouldBe true
       mayBeValid.leftMap { error =>
         error shouldBe a[RulesFileError]
         error.message shouldBe "Invalid answer value on row 4"
       }
     }
-
     "return error for invalid caryOver text" in {
-      val mayBeValid = validateRuleRow(RowFixture2.ruleRowWithInvalidCarryOverText, MetadataFixture2.metaData_withInvalidCarryOver, 2)
+      val (v,r) = (validRuleValues2,invalidExitResultColumns)
+      val mayBeValid = validateRuleRow(v ::: r, RulesFileMetaData(v.size, r.size, "", ""), 2)
       mayBeValid.isLeft shouldBe true
       mayBeValid.leftMap { error =>
         error shouldBe a[RulesFileError]
         error.message shouldBe "Invalid CarryOver value for row 2"
       }
     }
-
     "return error for invalid exit text" in {
-      val mayBeValid = SectionRuleValidator.validateRuleRow(RowFixture2.ruleRowWithInvalidExitText, MetadataFixture2.metaData_withInvalidExit, 2)
+      val (v,r) = (validRuleValues2,invalidResultColumns)
+      val mayBeValid = validateRuleRow(v ::: r, RulesFileMetaData(v.size, r.size, "", ""), 2)
       mayBeValid.isLeft shouldBe true
       mayBeValid.leftMap { error =>
         error shouldBe a[RulesFileError]
         error.message shouldBe "Invalid Exit value for row 2"
       }
-
     }
-
   }
-
 }
