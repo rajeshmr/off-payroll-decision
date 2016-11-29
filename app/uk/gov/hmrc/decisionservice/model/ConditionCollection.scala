6510f5da93458678c80963762485ced9b96fd83e
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2016-11-29 11:22:09 +0000

    changed the appName to off-payroll-decision

--- app/uk/gov/hmrc/decisionservice/model/ConditionCollection.scala
+++ app/uk/gov/hmrc/decisionservice/model/ConditionCollection.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.decisionservice.model
 
 abstract class ConditionCollection(conditions: Map[String,String]) {
@@ -27,4 +43,3 @@ case class PartOfOrganization(conditions: Map[String,String]) extends ConditionC
 case class PartOfOrganizationCarryOver(value:String) extends CarryOver
 
 case class Decision(text:String)
-
