6b74644844e1b802177d612326a41f23bc397738
REDACTED <REDACTED>
2017-02-13 16:50:52 +0000

    removed business structure from score

--- app/uk/gov/hmrc/decisionservice/model/api/ApiModel.scala
+++ app/uk/gov/hmrc/decisionservice/model/api/ApiModel.scala
@@ -33,7 +33,7 @@ object Score {
   def elements(version:String):List[String] = {
     val versionToElements = Map(
       "1.0.1-beta" -> List("control", "financialRiskA", "financialRiskB", "partAndParcel", "businessStructure", "personalService"),
-      "1.0.0-final" -> List("control", "financialRisk", "partAndParcel", "businessStructure", "personalService")
+      "1.0.0-final" -> List("control", "financialRisk", "partAndParcel", "personalService")
     )
     versionToElements.getOrElse(version, List())
   }
