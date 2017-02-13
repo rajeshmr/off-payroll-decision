e48cdb2c487bb42f12e5765e3d3a91add1e44c3f
REDACTED <REDACTED>
2017-02-13 14:36:42 +0000

    MofM 1.0.0-final uplift plus test

--- test/uk/gov/hmrc/decisionservice/services/DecisionServiceTestInstance.scala
+++ test/uk/gov/hmrc/decisionservice/services/DecisionServiceTestInstance.scala
@@ -39,6 +39,6 @@ object DecisionServiceTestInstance100final extends DecisionService {
     (15, "/tables/1.0.0-final/financial-risk.csv", "financialRisk"),
     (4, "/tables/1.0.0-final/part-and-parcel.csv", "partAndParcel"),
     (5, "/tables/1.0.0-final/personal-service.csv", "personalService"),
-    (5, "/tables/1.0.0-final/matrix-of-matrices.csv", "matrix")
+    (4, "/tables/1.0.0-final/matrix-of-matrices.csv", "matrix")
   ).collect{case (q,f,n) => RulesFileMetaData(q,f,n)}
 }
