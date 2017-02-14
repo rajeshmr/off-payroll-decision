1661503103995f45c4bd86613e051088916d6ffe
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2017-02-14 11:27:35 +0000

    Revert "Feature/offpay 465 with 486"

--- test/uk/gov/hmrc/decisionservice/controllers/PartAndParcelCsvSpec.scala
+++ test/uk/gov/hmrc/decisionservice/controllers/PartAndParcelCsvSpec.scala
@@ -30,9 +30,6 @@ class PartAndParcelCsvSpec extends UnitSpec with WithFakeApplication with Decisi
   val PART_AND_PARCEL_SCENARIO_3 = "/test-scenarios/single/part-and-parcel/scenario-3.csv"
   val PART_AND_PARCEL_SCENARIO_4 = "/test-scenarios/single/part-and-parcel/scenario-4.csv"
 
-  val PART_AND_PARCEL_SCENARIO_0_V2 = s"/test-scenarios/${Versions.VERSION2}/part-and-parcel/scenario-0.csv"
-  val PART_AND_PARCEL_SCENARIO_S_V2 = s"/test-scenarios/${Versions.VERSION2}/part-and-parcel/scenarios.csv"
-
   "POST /decide" should {
     "return 200 and correct response with the expected decision for part and parcel scenario 0" in {
       createRequestSendVerifyDecision(PART_AND_PARCEL_SCENARIO_0, Versions.VERSION1)
@@ -48,12 +45,6 @@ class PartAndParcelCsvSpec extends UnitSpec with WithFakeApplication with Decisi
     }
     "return 200 and correct response with the expected decision for part and parcel scenario 4" in {
       createRequestSendVerifyDecision(PART_AND_PARCEL_SCENARIO_4, Versions.VERSION1)
-    }
-    s"return 200 and correct response with the expected decision for part and parcel scenario 0 version ${Versions.VERSION2}" in {
-      createRequestSendVerifyDecision(PART_AND_PARCEL_SCENARIO_0_V2, Versions.VERSION2)
-    }
-    "return 200 and correct response with the expected decision for part and parcel scenarios version " + Versions.VERSION2 in {
-      createMultipleRequestsSendVerifyDecision(PART_AND_PARCEL_SCENARIO_S_V2, Versions.VERSION2)
     }
   }
 }
